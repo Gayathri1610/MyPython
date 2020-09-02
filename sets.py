b450ffd54089ad0d4e165c897271449631b5869b
Gayathri1610 <gayathrinalla0816@gmail.com>
2020-09-02 10:57:00 +0530

    sets:update

--- sets.py
+++ sets.py
@@ -3,3 +3,17 @@ thisset = {"apple", "banana", "cherry"}
 thisset.update(["orange", "mango", "grapes"])
 
 print(thisset)
+
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "facebook"}
+
+z = x.isdisjoint(y)
+
+print(z)
+
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+
+z = x.symmetric_difference(y)
+
+print(z)
