4dd836b794dcb423a53910e85dc255def41c3577
Gayathri1610 <gayathrinalla0816@gmail.com>
2020-09-02 11:09:58 +0530

    dict

--- 11dic.py
+++ 11dic.py
@@ -5,3 +5,20 @@ thisdict = {
 }
 thisdict.popitem()
 print(thisdict)
+
+myfamily = {
+  "child1" : {
+    "name" : "Emil",
+    "year" : 2004
+  },
+  "child2" : {
+    "name" : "Tobias",
+    "year" : 2007
+  },
+  "child3" : {
+    "name" : "Linus",
+    "year" : 2011
+  }
+}
+
+print(myfamily)
