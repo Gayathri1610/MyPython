89b0a27c1003965f5e3968ccf36b1a167420811b
Gayathri1610 <gayathrinalla0816@gmail.com>
2020-09-12 13:59:59 +0530

    arrays

---
+++ arrays.py
@@ -0,0 +1,36 @@
+cars = ["Ford", "Volvo", "BMW"]
+
+cars[0] = "Toyota"
+
+print(cars)
+
+cars = ["Ford", "Volvo", "BMW"]
+
+x = len(cars)
+
+print(x)
+
+cars = ["Ford", "Volvo", "BMW"]
+
+x = len(cars)
+
+print(x)
+
+fruits = ['apple', 'banana', 'cherry']
+
+fruits.reverse()
+
+print(fruits)
+
+cars = ['Ford', 'BMW', 'Volvo']
+
+cars.sort()
+
+print(cars)
+
+fruits = ['apple', 'banana', 'cherry']
+
+points = (1, 4, 5, 9)
+
+fruits.extend(points)
+
